ad29a0cb2c7f255905067d10bf7137ce1fbe0e10
IT21314742 <it21314742@my.sliit.lk>
2024-08-17 12:55:54 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -5,9 +5,8 @@ img = cv2.imread('assets/1.jpg', -1)
 
 print(img[257][400])
 
-for i in range(100):
-    for j in range(img.shape[1]):
-        img[i][j] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+tag = img[500:700, 600:900]
+img[100:300, 650:950] = tag
 
 cv2.imshow('Image', img)
 cv2.waitKey(0)
