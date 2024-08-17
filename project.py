20076d3f686b712864ca5c1ab59b0ede396d5b82
IT21314742 <it21314742@my.sliit.lk>
2024-08-17 12:42:41 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -9,7 +9,7 @@ for i in range(100):
     for j in range(img.shape[1]):
         img[i][j] = [random.randint(255), random.randint(255), random.randint(255)]
 
-cv2.imshow('Image', )
+cv2.imshow('Image', img)
 
 
 
