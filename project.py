9310e47f47ccaf3ea88aed0af71c5f5b32d170cb
IT21314742 <it21314742@my.sliit.lk>
2024-08-17 12:29:20 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -5,6 +5,7 @@ img = cv2.imread('assets/1.jpg', -1)
 print(img)
 
 [
-[[0, 0, 0], [255, 255, 255]],
+# regulary its gonna be rd green blue but in open CV its gonn be blue green red.
+[[0, 0, 0], [255, 255, 255]], 
 [[0, 0, 0], [255, 255, 255]],
 ]
