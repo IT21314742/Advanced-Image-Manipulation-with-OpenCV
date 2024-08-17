1bd44b26c899b4b23d4ca400eb0055045afdd657
IT21314742 <it21314742@my.sliit.lk>
2024-08-17 12:44:01 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ print(img[257][400])
 
 for i in range(100):
     for j in range(img.shape[1]):
-        img[i][j] = [random.randint(255), random.randint(255), random.randint(255)]
+        img[i][j] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
 
 cv2.imshow('Image', img)
 cv2.waitKey(0)
